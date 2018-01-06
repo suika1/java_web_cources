4138e14a9c86a0a14d9401998382061a317d50cb
Ivan_Kr <vanyoktank1@mail.ru>
2018-01-06 17:35:51 +0300

    added divison on zero and exit

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,11 @@
+/**
+ * Побочный класс
+ * @author Suika
+ * @since 05.01.2017
+ */
 public class Calculator{
     private int result;
-
+    private boolean divZer = false;
     //Adding
     public void add(int ... params){
         for (Integer param:params){
@@ -8,13 +13,54 @@ public class Calculator{
         }
     }
 
+    //choosing our action:
+    public void action(String s, int a, int b) {
+        this.result=0;
+        switch(s){
+            case "+":{
+                this.result = a+b;
+                break;
+            }
+            case "-":{
+                this.result = a-b;
+                break;
+            }
+            case "*":{
+                this.result = a*b;
+                break;
+            }
+            case "/":{
+                if (b==0) {
+                    this.divZer=true;
+                    break;
+                }else {
+                    this.result = a / b;
+                    break;
+                }
+            }
+            case "%":{
+                this.result = a%b;
+                break;
+            }
+            case "pow":{
+                this.result = (int)Math.pow(a,b);
+                break;
+            }
+        }
+    }
+
     //Getting Result
-    public int getResult(){
-        return this.result;
+    public void getResult(){
+        if (this.divZer==false){
+            System.out.println("Result is : "+this.result);
+        }else{
+            System.out.println("You tried to divide on 0, nothing happened");
+        }
     }
 
     //Cleaning result
     public void cleanResult(){
+        this.divZer=false;
         this.result =0;
     }
 }
