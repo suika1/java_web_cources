684132d04e5f00f398d8d76dd55b157b077f99f2
Ivan_Kr <vanyoktank1@mail.ru>
2018-01-13 01:33:23 +0300

    Added throwing exceptions

--- ClinicProject/src/main/java/ru/Clinic/lessons/Cat.java
+++ ClinicProject/src/main/java/ru/Clinic/lessons/Cat.java
@@ -8,20 +8,35 @@ public class Cat implements Pet {
 
     private final Animal animal;
 
+    /**
+     * Конструктор
+     * @param animal Объект класса Animal - текущий кот
+     */
     public Cat(final Animal animal){
         this.animal = animal;
     }
 
+    /**
+     * Получить имя
+     * @return Имя кота
+     */
     @Override
     public String getName() {
         return this.animal.getName();
     }
 
+    /**
+     * Издать звук
+     */
     @Override
     public void makeSound(){
         System.out.printf("%s said %s",this.getName(),"Hello, I'm cat, meow!");
     }
 
+    /**
+     * Установить имя
+     * @param name новое имя
+     */
     @Override
     public void setName(String name){
         this.animal.setName(name);
