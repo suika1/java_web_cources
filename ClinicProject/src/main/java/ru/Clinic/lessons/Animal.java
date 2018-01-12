2f48e9765814b66f55d7d3b5b40d38fac1e64b42
Ivan_Kr <vanyoktank1@mail.ru>
2018-01-13 01:27:07 +0300

    Added comments

--- ClinicProject/src/main/java/ru/Clinic/lessons/Animal.java
+++ ClinicProject/src/main/java/ru/Clinic/lessons/Animal.java
@@ -8,18 +8,33 @@ public class Animal implements Pet {
 
     private String name;
 
+    /**
+     * Конструктор
+     * @param name Имя
+     */
     public Animal(String name){
         this.name = name;
     }
 
+    /**
+     * Издать звук
+     */
     public void makeSound(){
         System.out.println(String.format("%s said: %s", this.name, "Hello, I'm an animal !"));
     }
 
+    /**
+     * Получить имя
+     * @return поле имя
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Установить имя
+     * @param name новое имя
+     */
     public void setName(String name){
         this.name = name;
     }
