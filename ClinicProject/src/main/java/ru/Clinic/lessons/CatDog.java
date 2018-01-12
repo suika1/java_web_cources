f328b0a702aa85d1e70187208c5b85d4e4895a9b
Ivan_Kr <vanyoktank1@mail.ru>
2018-01-13 01:31:32 +0300

    Added throwing exceptions

--- ClinicProject/src/main/java/ru/Clinic/lessons/CatDog.java
+++ ClinicProject/src/main/java/ru/Clinic/lessons/CatDog.java
@@ -9,23 +9,38 @@ public class CatDog implements Pet{
     private final Pet cat;
     private final Pet dog;
 
+    /**
+     * Конструктор котопса
+     * @param cat питомец-кот
+     * @param dog питомец-собака
+     */
     public CatDog(Pet cat, Pet dog){
         this.cat = cat;
         this.dog = dog;
     }
 
-
+    /**
+     * Получаем имя котопса
+     * @return Имя кота+Имя пса
+     */
     @Override
     public String getName() {
         return String.format("%s%s",this.cat.getName(),this.dog.getName());
     }
 
+    /**
+     * Издать звук
+     */
     @Override
     public void makeSound() {
         this.cat.makeSound();
         this.dog.makeSound();
     }
 
+    /**
+     * Установить имя
+     * @param name новое имя
+     */
     @Override
     public void setName(String name) {
         this.cat.setName(name);
