f9fde8ff09907287e4be4666aa56c9e3c893f871
Ivan_Kr <vanyoktank1@mail.ru>
2018-01-13 01:29:43 +0300

    Added comments

--- ClinicProject/src/main/java/ru/Clinic/lessons/Dog.java
+++ ClinicProject/src/main/java/ru/Clinic/lessons/Dog.java
@@ -8,20 +8,35 @@ public class Dog implements Pet {
 
     private final Animal animal;
 
+    /**
+     * Конструктор
+     * @param animal объект класса Animal - текущая собака
+     */
     public Dog(final Animal animal){
         this.animal = animal;
     }
 
+    /**
+     * Получить имя
+     * @return имя
+     */
     @Override
     public String getName() {
         return this.animal.getName();
     }
 
+    /**
+     * Издать звук
+     */
     @Override
     public void makeSound(){
         System.out.printf("%s said %s",this.getName(),"Hello, I'm a dog, gav-gav!");
     }
 
+    /**
+     * Установить имя
+     * @param name новое имя
+     */
     @Override
     public void setName(String name){
         this.animal.setName(name);
